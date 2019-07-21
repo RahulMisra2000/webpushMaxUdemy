23f08727bec973e755e426c931714043c74a42b0
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2019-07-21 15:04:41 -0400

    Creating and sending a subscription to our Server

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -27,24 +27,24 @@ window.addEventListener('beforeinstallprompt', function(event) {
 function displayConfirmNotification() {
   if ('serviceWorker' in navigator) {
     var options = {
-      body: 'You successfully subscribed to our Notification service!',
-      icon: '/src/images/icons/app-icon-96x96.png',
-      image: '/src/images/sf-boat.jpg',
-      dir: 'ltr',
-      lang: 'en-US', // BCP 47,
-      vibrate: [100, 50, 200],
-      badge: '/src/images/icons/app-icon-96x96.png',
-      tag: 'confirm-notification',
-      renotify: true,
-      actions: [
-        { action: 'confirm', title: 'Okay', icon: '/src/images/icons/app-icon-96x96.png' },
-        { action: 'cancel', title: 'Cancel', icon: '/src/images/icons/app-icon-96x96.png' }
-      ]
+          body: 'You successfully subscribed to our Notification service!',
+          icon: '/src/images/icons/app-icon-96x96.png',
+          image: '/src/images/sf-boat.jpg',
+          dir: 'ltr',
+          lang: 'en-US', // BCP 47,
+          vibrate: [100, 50, 200],
+          badge: '/src/images/icons/app-icon-96x96.png',
+          tag: 'confirm-notification',
+          renotify: true,
+          actions: [
+            { action: 'confirm', title: 'Okay', icon: '/src/images/icons/app-icon-96x96.png' },
+            { action: 'cancel', title: 'Cancel', icon: '/src/images/icons/app-icon-96x96.png' }
+          ]
     };
 
     navigator.serviceWorker.ready
       .then(function(swreg) {
-        swreg.showNotification('Successfully subscribed!', options);
+         swreg.showNotification('Successfully subscribed!', options);
       });
   }
 }
@@ -54,44 +54,51 @@ function configurePushSub() {
     return;
   }
 
-  var reg;
+  var reg;                                            // *** declaring it here so that it can be used outside the function ************
   navigator.serviceWorker.ready
-    .then(function(swreg) {
-      reg = swreg;
-      return swreg.pushManager.getSubscription();
+    .then(function(swreg) {                           // ********** SW registration object
+          reg = swreg;
+          return swreg.pushManager.getSubscription();   // ******** Asking the Push Manager for the subscription object
     })
     .then(function(sub) {
-      if (sub === null) {
-        // Create a new subscription
-        var vapidPublicKey = 'BKapuZ3XLgt9UZhuEkodCrtnfBo9Smo-w1YXCIH8YidjHOFAU6XHpEnXefbuYslZY9vtlEnOAmU7Mc-kWh4gfmE';
-        var convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
-        return reg.pushManager.subscribe({
-          userVisibleOnly: true,
-          applicationServerKey: convertedVapidPublicKey
-        });
-      } else {
-        // We have a subscription
-      }
+          if (sub === null) {                             // No subscription so, Create a new subscription
+              
+              // ******** user npm package web-push to create the public private key pair from the command line
+              //          This is the public key of that pair 
+              var vapidPublicKey = 'BKapuZ3XLgt9UZhuEkodCrtnfBo9Smo-w1YXCIH8YidjHOFAU6XHpEnXefbuYslZY9vtlEnOAmU7Mc-kWh4gfmE';
+              var convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
+              return reg.pushManager.subscribe({                    // ******** subscribe
+                    userVisibleOnly: true,
+                    applicationServerKey: convertedVapidPublicKey   
+              });
+          } else {                                      // ******  We already have a subscription
+          }
     })
     .then(function(newSub) {
-      return fetch('https://pwagram-99adf.firebaseio.com/subscriptions.json', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        },
-        body: JSON.stringify(newSub)
-      })
-    })
+          // ******* send the subscription object to our Application Server ****************************************** 
+          // ******* Max used the Firebase backend. It is really a BaaS ... backend as a service
+          // ******* It is a real-time database and we can write functions that get exposed as an API to the outside world
+          //         The one below is an example 
+          return fetch('https://pwagram-99adf.firebaseio.com/subscriptions.json', {
+                      method: 'POST',
+                      headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                      },
+                      body: JSON.stringify(newSub)    
+                    })
+        })
     .then(function(res) {
-      if (res.ok) {
-        displayConfirmNotification();
-      }
+          if (res.ok) {                       //******** IF fetch was all good then 
+            displayConfirmNotification();     //******* Let the user know that they are subscribed
+          }
     })
     .catch(function(err) {
-      console.log(err);
+          console.log(err);
     });
 }
+
+
 
 function askForNotificationPermission() {
   Notification.requestPermission(function(result) {
